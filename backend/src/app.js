cb9f0c5abfdf8b441de3f7f489d6fb392c59bae3
jaidev3 <91534937+jaidev3@users.noreply.github.com>
2021-12-16 20:20:46 +0530

    extra

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,23 @@
-const express = require("express");
 
+//////////////imports//////////////////
+const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const dotenv = require('dotenv');
 const storeController = require("./controllers/store.controller");
 const itemController = require("./controllers/item.controller");
-
 const app = express();
-app.use(express.json());
 
+////////configs/////////////////
+dotenv.config();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+
+
+app.get('/', (req,res) => {
+    res.send("This is backend")
+})
 app.use("/store", storeController)
 app.use("/item", itemController)
 
