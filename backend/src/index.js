7a67cabd737b06b4fc77986b037b76eefc8dc580
Vijay Kumar Singh <vijay_fw11_204@masai.school>
2021-12-18 17:57:49 +0530

    fina

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ const app = require("./app")
 
 const connect = require("./configs/db")
 
+///////////////////////////////////
+
+///////////////////////////////
+
 app.listen(process.env.PORT || 2345, async function(){
     await connect();
     console.log("listening on port 2345")
